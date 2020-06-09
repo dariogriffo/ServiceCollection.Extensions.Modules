fa170341158a2a801e713bcedf31c3e83ae2954e
Dario Griffo <dariogriffo@gmail.com>
2020-06-09 10:37:45 +0200

    Removing not-allowed duplicates functionality. Fix issue with services already registered not present.

--- src/ServiceCollection.Extensions.Modules/Module.cs
+++ src/ServiceCollection.Extensions.Modules/Module.cs
@@ -1,14 +1,9 @@
 ﻿namespace ServiceCollection.Extensions.Modules
 {
-    using System;
-    using System.Collections.Generic;
     using Microsoft.Extensions.DependencyInjection;
 
     public abstract class Module
     {
-        private static readonly HashSet<Type> LoadedModules = new HashSet<Type>();
-        private readonly IServiceCollection _childServices = new ServiceCollection();
-
         protected internal Module()
         {
         }
@@ -20,21 +15,7 @@
         internal IServiceCollection Loader<T>(IServiceCollection services, T module)
             where T : Module
         {
-            var type = module.GetType();
-            if (LoadedModules.Contains(type))
-            {
-                // Avoid duplicated load
-                return services;
-            }
-
-            LoadedModules.Add(type);
-
-            Load(_childServices);
-            foreach (var sd in _childServices)
-            {
-                services.Add(sd);
-            }
-
+            Load(services);
             return services;
         }
     }
--- tests/ServiceCollection.Extensions.Modules.UnitTests/ModuleExtensionsTests.cs
+++ tests/ServiceCollection.Extensions.Modules.UnitTests/ModuleExtensionsTests.cs
@@ -4,7 +4,6 @@
     using System.Linq;
     using FluentAssertions;
     using Microsoft.Extensions.DependencyInjection;
-    using ServiceCollection.Extensions.Modules.UnitTests.Helpers;
     using ServiceCollection.Extensions.Modules.UnitTests.Interfaces;
     using ServiceCollection.Extensions.Modules.UnitTests.Modules;
     using NUnit.Framework;
@@ -12,50 +11,6 @@
     [TestFixture]
     public class ModuleExtensionsTests
     {
-        [Test]
-        public void RegisterModule_WithGeneric_DuplicatesOnlyLoadsOnce()
-        {
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.RegisterModule<TestModule1>();
-            serviceCollection.RegisterModule<TestModule1>();
-
-            using var scope1 = serviceCollection.BuildServiceProvider();
-            scope1
-                .GetRequiredService<IService>()
-                .Message()
-                .Should().Be("1");
-
-            using var scope2 = serviceCollection.BuildServiceProvider();
-            scope2
-                .GetRequiredService<IService>()
-                .Message()
-                .Should().Be("1");
-
-            TestModule1.LoadedCount.Should().Be(1);
-        }
-
-        [Test]
-        public void RegisterModule_WithInstance_DuplicatesOnlyLoadsOnce()
-        {
-            var counter = new CountHolder();
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.RegisterModule(new TestModule2(counter));
-            serviceCollection.RegisterModule(new TestModule2(counter));
-
-            using var scope1 = serviceCollection.BuildServiceProvider();
-            scope1
-                .GetRequiredService<IService>()
-                .Message()
-                .Should().Be("2");
-
-            using var scope2 = serviceCollection.BuildServiceProvider();
-            scope2
-                .GetRequiredService<IService>()
-                .Message()
-                .Should().Be("2");
-
-            counter.Count.Should().Be(1);
-        }
 
         [Test]
         public void RegisterModule_WithNestedModules_AllDependenciesAreRegistered()
